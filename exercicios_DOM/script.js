a5c0446e43efa0e0b76699baf4ff6989712f9488
thomas <thomas@qpix.com.br>
2025-05-15 22:14:35 -0300

    Primeiro exercício sobre DOM - Fazer um input e um botão onde o usuário coloca uma tarefa, e adiciona a lista, um botão de deletar vai aparecer junto com a tarefa

---
+++ exercicios_DOM/script.js
@@ -0,0 +1,14 @@
+//Primeiro exercício
+
+taskForm.onsubmit = (event) => {
+    event.preventDefault();
+    const li = document.createElement('li');
+    li.textContent = taskInput.value;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = 'Deletar';
+    deleteButton.onclick = () => li.remove();
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+    taskInput = "";
+}
